8a1b06c2cf678622f8dba6889b6ac5a27de04839
Carlos Marx <carlosmarx27@gmail.com>
2022-05-06 13:26:36 -0300

    Remove try and catch from routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,23 +8,17 @@ export const routes = express.Router()
 routes.post('/feedbacks', async (req, res) => {
   const {type, comment, screenshot} = req.body
 
-  try {
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const nodemailerMailAdapter = new NodemailerMailAdapter()
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository, nodemailerMailAdapter
+  )
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-      prismaFeedbacksRepository, nodemailerMailAdapter
-      )
-      
-      await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot, 
-      })
-      
-      return res.status(201).send()
-  } catch (err) {
-    console.log(err)
-    return res.status(500).send()
-  }
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot, 
+  })
+
+  return res.status(201).send()
 })
